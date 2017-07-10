d14dd88a9c9e33eb1b786c47acb7e479d6701bf6
ViktorStefanyk <viktor.stephanyk@gmail.com>
2017-07-10 17:49:50 +0300

    commit the war file

--- src/main/java/com/web/sj/repository/IAddProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.web.sj.repository;
-
-import com.web.sj.domain.Product;
-
-public interface IAddProductRepository {
-	
-	public void addProduct(Product product);
-
-}
--- src/main/java/com/web/sj/repository/IImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.web.sj.repository;
-
-public interface IImageRepository {
-	
-	public void addMainImage(Integer productId, String imageName, String imageRole);
-
-}
